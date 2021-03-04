653c4a906e15a80333acf5833d4e55ab805928cb
Ben Smith <bensmith@tributemedia.com>
2021-03-04 15:36:01 -0700

    Moved bar() below colorText() and updated warning messages

--- headerFunctions.sh
+++ headerFunctions.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-# Function bar takes in 1 required input and up to 2 option inputs
-#  The purpose of this function is to print out a character across the width
-#  of the current terminal window
-# Input $1 (String) is the character to be printed. Needs to be only 1 character
-#  long. There is internal validation for this.
-# Input $2 (boolean) determines whether to add a trailing new line character
-#  or not after the bar of characters has been printed. Defaults to TRUE
-# Input $3 (int) is an optional input if a custom width is needed that isn't
-#  the width of the current terminal window
-function bar() {
-  termWidth=$(tput cols)
-  strlen=${#1}
-
-  if [[ $strlen -gt 1 ]]; then
-    printf "[WARNING] Parameter \$1 of function bar()"
-    printf "can only be 1 character long\n"
-    return
-  fi
-
-  if [[ $3 -gt 0 ]]; then
-    width=$3
-  else
-    width=$termWidth
-  fi
-
-  for (( i = 0; i < $width; i++ )); do
-    printf $1
-  done
-
-  if [[ $2 = true ]]; then
-    echo
-  fi
-}
-
 # This function takes in a text string and uses ANSI escape codes to change the
 #  the color of the text. To do this the function takes in 2 or 3 inputs.
 #
@@ -114,7 +80,7 @@ function colorText() {
     local background=";41"
   elif [[ $3 == "green" ]]; then
     local background=";42"
-  elif [[ $3 == "yellow"] ]]; then
+  elif [[ $3 == "yellow" ]]; then
     local background=";43"
   elif [[ $3 == "blue" ]]; then
     local background=";44"
@@ -137,4 +103,39 @@ function colorText() {
 
   local text="\033[${foreground}${background}m${1}${killColors}"
   echo -en $text
+}
+
+# Function bar takes in 1 required input and up to 2 option inputs
+#  The purpose of this function is to print out a character across the width
+#  of the current terminal window
+# Input $1 (String) is the character to be printed. Needs to be only 1 character
+#  long. There is internal validation for this.
+# Input $2 (boolean) determines whether to add a trailing new line character
+#  or not after the bar of characters has been printed. Defaults to TRUE
+# Input $3 (int) is an optional input if a custom width is needed that isn't
+#  the width of the current terminal window
+function bar() {
+  termWidth=$(tput cols)
+  strlen=${#1}
+
+  if [[ $strlen -gt 1 ]]; then
+    printf "$(colorText "[WARNING]" "yellow") Parameter "
+    printf "$(colorText "\$1" "red") of function "
+    printf "$(colorText "bar" "cyan")() can only be 1 character long\n"
+    return
+  fi
+
+  if [[ $3 -gt 0 ]]; then
+    width=$3
+  else
+    width=$termWidth
+  fi
+
+  for (( i = 0; i < $width; i++ )); do
+    printf $1
+  done
+
+  if [[ $2 = true ]]; then
+    echo
+  fi
 }
